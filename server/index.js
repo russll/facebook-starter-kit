2c6050dc26d199cc47811090115462acffdeb60c
russel <wb.russel@gmail.com>
2016-01-18 23:34:33 +0300

    Fix port on main server

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ import path from 'path';
 import express from 'express';
 
 const isDeveloping = process.env.NODE_ENV !== 'production';
-const port = isDeveloping ? 8080 : process.env.PORT;
+const port = isDeveloping ? 8000 : process.env.PORT;
 const address = process.env.DOCKER_HOST || process.env.APP_IP || 'localhost';
 const app = express();
 
