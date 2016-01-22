56dfd974ea7d41d09cb6b521409d66c2b4f37e23
russel <wb.russel@gmail.com>
2016-01-22 23:18:00 +0300

    Fix graphql server

--- server/graphql.js
+++ server/graphql.js
@@ -1,13 +1,11 @@
-import path from 'path';
 import express from 'express';
+import graphQLHTTP from 'express-graphql';
+import { schema } from '../data/schema.js';
 
 const isDeveloping = process.env.NODE_ENV !== 'production';
 const port = isDeveloping ? 8080 : process.env.PORT;
 const address = process.env.DOCKER_HOST || process.env.APP_IP || 'localhost';
 const app = express();
-
-import graphQLHTTP from 'express-graphql';
-import { schema } from '../data/schema.js';
 
 app.use('/', graphQLHTTP((request) => ({
   graphiql: isDeveloping,
